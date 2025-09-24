2abb2bbdc97a5b42e9675e1ed87f3c2d9ea0dbfa
Gustavo Yance <gusyancab@gmail.com>
2025-09-23 20:52:09 -0500

    chore: bump version to 0.0.8

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-export * from "./components/atoms/Badge/Badge";
-export * from "./components/atoms/Button/Button";
-export * from "./components/atoms/Input/Input";
-export * from "./components/atoms/Label/Label";
-export * from "./components/atoms/Typography/Typography";
-export * from "./components/molecules/ComboBoxField/ComboBoxField";
-export * from "./components/molecules/TextField/TextField";
+export * from "./components/atoms/Badge";
+export * from "./components/atoms/Button";
+export * from "./components/atoms/Input";
+export * from "./components/atoms/CheckBox";
+export * from "./components/atoms/Label";
+export * from "./components/atoms/Switch";
+export * from "./components/atoms/Typography";
+
+export * from "./components/molecules/ComboBoxField";
+export * from "./components/molecules/TextField";
+export * from "./components/molecules/CheckBoxField";
+export * from "./components/molecules/FieldGroup";
+export * from "./components/molecules/MultiCheckBoxField";
+
+export * from "./components/organisms/Form";
